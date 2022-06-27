59f20c7fd9f672a21b7d8752dce04d90ec433839
StefanoZanoni <stefano.zanoni00@gmail.com>
2022-06-27 16:56:48 +0200

    Initial commit

---
+++ src/base/Post.java
@@ -0,0 +1,190 @@
+package base;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Math.*;
+
+public final class Post {
+
+    private static class Comment {
+
+        private final String author;
+        private final String text;
+        private final String timestamp;
+
+        public Comment(String author, String text) {
+
+            this.author = author;
+            this.text = text;
+            timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().getTime());
+
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+
+    }
+
+    private final AtomicInteger ID = new AtomicInteger(0);
+    private final String owner;
+    private final String title;
+    private final String content;
+    private final List<String> shares = new LinkedList<>();
+    private final List<String> upvotes = new LinkedList<>();
+    private final List<String> downvotes = new LinkedList<>();
+    private final List<Comment> comments = new LinkedList<>();
+
+    public Post(String owner, String title, String content) {
+
+        if (owner == null || title == null || content == null)
+            throw new NullPointerException();
+
+        ID.getAndIncrement();
+        this.owner = owner;
+        this.title = title;
+        this.content = content;
+
+    }
+
+    public int getID() {
+        return ID.get();
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public synchronized void addShare(String username) {
+        shares.add(username);
+    }
+
+    public List<String> getShares() {
+        return shares;
+    }
+
+    public synchronized void addUpvote(String username) {
+        upvotes.add(username);
+    }
+
+    public List<String> getUpvotes() {
+        return upvotes;
+    }
+
+    public synchronized void addDownvote(String username) {
+        downvotes.add(username);
+    }
+
+    public List<String> getDownvotes() {
+        return downvotes;
+    }
+
+    public synchronized void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return Objects.equals(getID(), post.getID()) && getOwner().equals(post.getOwner())
+                && getTitle().equals(post.getTitle()) && getContent().equals(post.getContent())
+                && getShares().equals(post.getShares()) && getUpvotes().equals(post.getUpvotes())
+                && getDownvotes().equals(post.getDownvotes()) && getComments().equals(post.getComments());
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getID(), getOwner(), getTitle(), getContent());
+    }
+
+    /**
+     * @param numberOfPreviousUpvotes   the number of previous upvotes that is stored by the server
+     * @param numberOfPreviousDownvotes the number of previous downvotes that is stored by the server
+     * @param numberOfPreviousComments  the number of previous comments that is stored by the server
+     * @param age                       the number of times the reward was computed
+     * @return the computed reward for this post
+     * <p>
+     * Since a user cannot upvote or downvote two times the same post and
+     * upvote and downvote can only assume the value +1 and -1 respectively, the first summation
+     * inside the first log is well-defined
+     */
+    public float computeReward(int numberOfPreviousUpvotes, int numberOfPreviousDownvotes,
+                               int numberOfPreviousComments, int age) {
+
+        return (float) ((log(max((upvotes.size() - numberOfPreviousUpvotes - (downvotes.size() - numberOfPreviousDownvotes)), 0) + 1)
+                + log((computeCommentsValue(numberOfPreviousComments)) + 1)) / age);
+
+    }
+
+    /**
+     * @param numberOfPreviousComments the number of previous comments that is stored by the server
+     * @return the total value of new comments
+     * <p>
+     * The amount of comments of every user is computed with the help of a HashMap
+     */
+    private float computeCommentsValue(int numberOfPreviousComments) {
+
+        float commentsValue = 0;
+
+        class UserCounter {
+            int value = 1;
+
+            public void increment() {
+                value++;
+            }
+
+            public int getValue() {
+                return value;
+            }
+        }
+
+        Map<String, UserCounter> peopleChecked = new HashMap<>();
+
+        for (Comment comment : comments.subList(numberOfPreviousComments + 1, comments.size())) {
+
+            String author = comment.getAuthor();
+            UserCounter userCounter = peopleChecked.get(author);
+            if (userCounter == null)
+                peopleChecked.put(author, new UserCounter());
+            else
+                userCounter.increment();
+
+        }
+
+        for (Comment comment : comments.subList(numberOfPreviousComments + 1, comments.size())) {
+
+            String author = comment.getAuthor();
+            commentsValue += (2 / (1 + exp(1 - peopleChecked.get(author).getValue())));
+
+        }
+
+        return commentsValue;
+
+    }
+
+}
---
+++ src/base/Wallet.java
@@ -0,0 +1,56 @@
+package base;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public final class Wallet {
+
+    private static class Transaction {
+
+        private final float increment;
+        private final String timestamp;
+
+        public Transaction(float increment) {
+
+            this.increment = increment;
+            timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        }
+
+        public String getTimestamp() { return timestamp; }
+
+        public float getIncrement() { return increment; }
+
+    }
+
+    private float rewards = 0;
+    private final List<Transaction> transactionsHistory = new LinkedList<>();
+
+    public void addTransaction(Transaction transaction) {
+
+        transactionsHistory.add(transaction);
+        rewards += transaction.getIncrement();
+
+    }
+
+    public List<Transaction> getTransactions() { return transactionsHistory; }
+
+    public float getRewards() { return rewards; }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof Wallet wallet)) return false;
+        return Float.compare(wallet.getRewards(), getRewards()) == 0
+                && transactionsHistory.equals(wallet.transactionsHistory);
+
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(getRewards()); }
+
+}
