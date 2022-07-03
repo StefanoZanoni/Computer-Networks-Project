ce977b778ddae5272897dd534a4226526cb040f2
StefanoZanoni <stefano.zanoni00@gmail.com>
2022-07-03 19:08:40 +0200

    added sending requests

--- src/Winsome_client/src/TCPConnectionManager.java
+++ src/Winsome_client/src/TCPConnectionManager.java
@@ -1,10 +1,16 @@
-import commands.CommandParser;
+import commands.UnknownCommandException;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TCPConnectionManager {
     SocketChannel socketChannel;
@@ -30,33 +36,33 @@ public class TCPConnectionManager {
 
     }
 
-    public void sendCommand(String command, List<String> arguments) {
+    public void sendCommand(String command, List<String> arguments) throws UnknownCommandException {
 
          class CommandSelector {
 
-            public void select(String command, List<String> arguments) {
+            public void select(String command, List<String> arguments) throws UnknownCommandException {
 
                 switch (command) {
-                    case "register" ->
-                            register(arguments.get(0), arguments.get(1).toCharArray(), arguments.subList(2, arguments.size()));
-                    case "login" -> login(arguments.get(0), arguments.get(1).toCharArray());
-                    case "logout" -> logout(arguments.get(0));
-                    case "list users" -> listUsers();
-                    case "list followers" -> listFollowers();
-                    case "list following" -> listFollowing();
-                    case "follow" -> followUser(Integer.parseInt(arguments.get(0)));
-                    case "unfollow" -> unfollowUser(Integer.parseInt(arguments.get(0)));
-                    case "blog" -> viewBlog();
-                    case "post" -> createPost(arguments.get(0), arguments.get(1));
-                    case "show feed" -> showFeed();
-                    case "show post" -> showPost(Integer.parseInt(arguments.get(0)));
-                    case "delete" -> deletePost(Integer.parseInt(arguments.get(0)));
-                    case "rewin" -> rewinPost(Integer.parseInt(arguments.get(0)));
-                    case "rate" -> ratePost(Integer.parseInt(arguments.get(0)), Integer.parseInt(arguments.get(1)));
-                    case "comment" -> addComment(Integer.parseInt(arguments.get(0)), arguments.get(1));
-                    case "wallet" -> getWallet();
-                    case "wallet btc" -> getWalletBitcoin();
+                    case "register" -> register(arguments.get(0), arguments.get(1).toCharArray(),
+                            arguments.subList(2, arguments.size()));
+
+                    case "login", "post" -> send(command, arguments.get(0), arguments.get(1));
+
+                    case "logout" -> send(command, arguments.get(0));
+
+                    case "list users", "list followers", "list following",
+                            "blog", "show feed", "wallet", "wallet btc" -> send(command);
+
+                    case "follow", "unfollow", "show post",
+                            "delete", "rewin" -> send(command, Integer.parseInt(arguments.get(0)));
+
+                    case "rate" -> send(command, Integer.parseInt(arguments.get(0)), Integer.parseInt(arguments.get(1)));
+
+                    case "comment" -> send(command, Integer.parseInt(arguments.get(0)), arguments.get(1));
+
+                    default -> throw new UnknownCommandException(command + "is not a valid command");
                 }
+
             }
 
         }
@@ -66,75 +72,111 @@ public class TCPConnectionManager {
 
     }
 
+    private void send(String command, Object... arguments) {
+
+        int bufferDim;
+        if (arguments.length == 0)
+            bufferDim = command.length();
+        else {
+            bufferDim = command.length() + Character.BYTES;
+            int argumentsDim = (arguments.length - 1) * Character.BYTES;
+            for (Object object : arguments) {
+                if (object instanceof Integer)
+                    argumentsDim += Integer.BYTES;
+                else if (object instanceof String)
+                    argumentsDim += ((String) object).length();
+                else if (object instanceof char[])
+                    argumentsDim += ((char[]) object).length;
+            }
+            bufferDim += argumentsDim;
+        }
+
+        ByteBuffer buffer = ByteBuffer.allocate(bufferDim);
+
+        buffer.put( command.getBytes(StandardCharsets.UTF_8) );
+        buffer.put( "|".getBytes(StandardCharsets.UTF_8) );
+        for (Object object : arguments) {
+            if (object instanceof Integer) {
+                buffer.put( ((Integer) object).byteValue() );
+                buffer.put( "|".getBytes(StandardCharsets.UTF_8) );
+            }
+            else if (object instanceof String) {
+                buffer.put( ((String) object).getBytes(StandardCharsets.UTF_8) );
+                buffer.put( "|".getBytes(StandardCharsets.UTF_8) );
+            }
+            else if (object instanceof char[]) {
+                buffer.put( Arrays.toString(((char[]) object)).getBytes(StandardCharsets.UTF_8) );
+                buffer.put( "|".getBytes(StandardCharsets.UTF_8) );
+            }
+        }
+
+        try {
+            socketChannel.write(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException("I/O error while sending command to server");
+        }
+
+
+    }
+
     private void register(String username, char[] password, List<String> tags) {
 
-    }
+        if (username == null || password == null || tags == null)
+            throw new NullPointerException();
 
-    private void login(String username, char[] password) {
+        if (tags.size() > 5) {
+            System.out.println("have been inserted over 5 tags, only the first 5 will be maintain\n");
+            tags.subList(5, tags.size()).clear();
+        }
 
-    }
+        while (!validateUsername(username)) {
 
-    private void logout(String username) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n\ninvalid username, please enter a new username respecting the following format\n\n");
+            System.out.println("1) username must contains only alphanumeric characters, underscore and dot\n");
+            System.out.println("2) underscore and dot can't be at the end or start of the username\n");
+            System.out.println("3) underscore and dot can't be next to each other\n");
+            System.out.println("4) underscore or dot can't be used multiple times in a row\n");
+            System.out.println("5) number of characters must be between 8 to 20\n");
+            System.out.println("> ");
+            username = scanner.next();
 
-    }
+        }
 
-    private void listUsers() {
+        while (!validatePassword(password)) {
 
-    }
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n\ninvalid password, please enter a new password respecting the following format\n\n");
+            System.out.println("1) password must contains at least 8 characters\n");
+            System.out.println("2) contains at least one digit\n");
+            System.out.println("3) contains at least one lower case letter and one upper case letter\n");
+            System.out.println("4) a special character must occur at least once\n");
+            System.out.println("5) no whitespace allowed in the entire password\n");
+            System.out.println("> ");
+            password = scanner.next().toCharArray();
 
-    private void listFollowers() {
+        }
 
-    }
-
-    private void listFollowing() {
-
-    }
-
-    private void followUser(int idUser) {
-
-    }
-
-    private void unfollowUser(int idUser) {
-
-    }
-
-    private void viewBlog() {
-
-    }
-
-    private void createPost(String title, String content) {
-
-    }
-
-    private void showFeed() {
-
-    }
-
-    private void showPost(int idPost) {
+        send("register", username, password, tags.get(0),
+                tags.get(1), tags.get(2), tags.get(3), tags.get(4));
 
     }
 
-    private void deletePost(int idPost) {
+    private boolean validateUsername(String username) {
+
+        String regex = "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z\\d._]+(?<![_.])$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(username);
+        return matcher.matches();
 
     }
 
-    private void rewinPost(int idPost) {
+    private boolean validatePassword(char[] password) {
 
-    }
-
-    private void ratePost(int idPost, int vote) {
-
-    }
-
-    private void addComment(int idPost, String comment) {
-
-    }
-
-    private void getWallet() {
-
-    }
-
-    private void getWalletBitcoin() {
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(CharBuffer.wrap(password));
+        return matcher.matches();
 
     }
 
