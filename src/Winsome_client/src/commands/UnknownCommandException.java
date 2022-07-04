3c538fe9722873503f75c0de4dae76de450d766f
StefanoZanoni <stefano.zanoni00@gmail.com>
2022-07-04 16:42:02 +0200

    update

--- src/Winsome_client/src/commands/UnknownCommandException.java
+++ src/Winsome_client/src/commands/UnknownCommandException.java
@@ -2,6 +2,8 @@ package commands;
 
 public class UnknownCommandException extends Exception {
 
+    public UnknownCommandException() { super(); }
+
     public UnknownCommandException(String e) { super(e); }
 
 }
